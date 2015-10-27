193c8e0bd8ab0739af799074fb30eb542e8113df
Natalia21 <Natalia1992.92@mail.ru>
2015-10-27 15:09:13 +0300

    comment

--- JS/index.js
+++ JS/index.js
@@ -4,16 +4,15 @@ $( document ).ready(function() {
 	    loop:true,
 	    margin:10,
 	    nav:true,
+	    navText: ['natasha', 'olya'],
 	    responsive:{
 	        0:{
 	            items:1
 	        },
 	        600:{
 	            items:3
-	        },
-	        1000:{
-	            items:5
 	        }
+	        
 	    }
 	})
 
